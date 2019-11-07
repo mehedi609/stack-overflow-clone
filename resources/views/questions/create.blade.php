d7e4705ea35ead264c1256da5cf617f1c4c93189
mehedi609 <35611282+mehedi609@users.noreply.github.com>
2019-11-08 00:03:47 +0600

    "Create and show Question form"

---
+++ resources/views/questions/create.blade.php
@@ -0,0 +1,56 @@
+@extends('layouts.app')
+
+@section('content')
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-12">
+        <div class="card">
+          <div class="card-header">
+
+            <div class="d-flex align-items-center">
+              <h2>All Questions</h2>
+              <div class="ml-auto">
+                <a href="{{route('questions.index')}}" class="btn btn-outline-secondary">
+                  Back to All Questions
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div class="card-body">
+
+            <form action="{{route('questions.store')}}" method="post">
+              @csrf
+
+              <div class="form-group">
+                <label for="question-title">Question Title</label>
+                <input type="text"
+                       class="form-control @error ('title') is-invalid @enderror" name="title" id="question-title"
+                       placeholder="Enter Question Title"
+                >
+                @if ($error->has('title'))
+                    <div class="invalid-feedback">
+                      <strong>{{$error->first('title')}}</strong>
+                    </div>
+                @endif
+              </div>
+
+              <div class="form-group">
+                <label for="question-body">Explain your Question</label>
+                <textarea class="form-control @error ('body') is-invalid @enderror" name="body" id="question-body" rows="10"></textarea>
+              </div>
+              @if ($error->has('body'))
+                <div class="invalid-feedback">
+                  <strong>{{$error->first('body')}}</strong>
+                </div>
+              @endif
+
+              <button type="submit" class="btn btn-outline-primary btn-lg">Ask this Question</button>
+            </form>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+@endsection
