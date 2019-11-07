98c0ac24b89d9a5adff7d107b086b0e9ce73c3d5
mehedi609 <35611282+mehedi609@users.noreply.github.com>
2019-11-07 18:34:43 +0600

    "Populate users and questions table using model factory and seed."

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Question;
+use App\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,5 +14,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+      factory(User::class, 3)->create()->each(function ($u) {
+        $u->questions()
+          ->saveMany(
+            factory(Question::class, rand(5, 10))->make()
+          );
+      });
     }
 }
