f902737f2e6ea47877856e9256710cba7ed18923
mehedi609 <35611282+mehedi609@users.noreply.github.com>
2019-11-08 20:44:24 +0600

    "Create AnswerFactory and Generate fake answers for each questions."

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,24 +1,30 @@
 <?php
 
+use App\Answer;
 use App\Question;
 use App\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        // $this->call(UsersTableSeeder::class);
-      factory(User::class, 3)->create()->each(function ($u) {
-        $u->questions()
-          ->saveMany(
-            factory(Question::class, rand(5, 10))->make()
-          );
-      });
-    }
+  /**
+   * Seed the application's database.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    // $this->call(UsersTableSeeder::class);
+    factory(User::class, 3)->create()->each(function ($u) {
+      $u->questions()
+        ->saveMany(
+          factory(Question::class, rand(1, 5))->make()
+        )->each(function ($q) {
+          $q->answers()
+            ->saveMany(
+              factory(Answer::class, rand(1, 5))->make()
+            );
+        });
+    });
+  }
 }
